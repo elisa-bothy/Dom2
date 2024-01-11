270e7152cf607f576dad26d00c4c51afe9093d0d
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-11 10:52:30 +0100

    2e essai, ne focntionne toujours pas, mais il n'y plus d'erreur synthaxique

--- assets/js/exoDom2.2.js
+++ assets/js/exoDom2.2.js
@@ -18,21 +18,24 @@ const main = document.createElement("main");
 const div = document.createElement("div");
 const p = document.createElement("p");
 const ul = document.createElement("ul")
-const creationli = [li1, li2, li3, li4, li5];
+const creationli = ["li1", "li2", "li3", "li4", "li5"];
+const contenuli =["Pomme", "Poire", "Haricots verts", "Carottes", "Concombre"];
 for(let i=0; i<5; i++){
     creationli[i] = document.createElement("li");
+    creationli[i].textContent = contenuli[i];
 }
 
 p.textContent = "Mangez 5 fruits et légumes par jour :";
-li1.textContent = "Pomme";
+/* li1.textContent = "Pomme";
 li2.textContent = "Poire";
 li3.textContent = "Haricots verts";
 li4.textContent = "Carottes";
-li5.textContent = "Concombre";
+li5.textContent = "Concombre"; */
 
 div.id = "2c3"
-
-ul.append(li1, li2, li3, li4, li5);
+for(let i=0; i<5; i++){
+ul.append(creationli[i]);
+}
 div.appendChild(p, ul);
 main.appendChild(div);
 
