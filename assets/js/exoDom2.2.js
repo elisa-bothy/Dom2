6239d9068e691081f62942a0021dcb0b5ff00931
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-11 11:34:16 +0100

    2e exo

--- assets/js/exoDom2.2.js
+++ assets/js/exoDom2.2.js
@@ -18,25 +18,17 @@ const main = document.createElement("main");
 const div = document.createElement("div");
 const p = document.createElement("p");
 const ul = document.createElement("ul")
-const creationli = ["li1", "li2", "li3", "li4", "li5"];
 const contenuli =["Pomme", "Poire", "Haricots verts", "Carottes", "Concombre"];
 for(let i=0; i<5; i++){
-    creationli[i] = document.createElement("li");
-    creationli[i].textContent = contenuli[i];
+    const li = document.createElement("li");
+    li.textContent = contenuli[i];
+    ul.appendChild(li);
 }
 
 p.textContent = "Mangez 5 fruits et légumes par jour :";
-/* li1.textContent = "Pomme";
-li2.textContent = "Poire";
-li3.textContent = "Haricots verts";
-li4.textContent = "Carottes";
-li5.textContent = "Concombre"; */
 
 div.id = "2c3"
-for(let i=0; i<5; i++){
-ul.append(creationli[i]);
-}
-div.appendChild(p, ul);
+div.append(p, ul);
 main.appendChild(div);
 
 document.body.insertBefore(main, document.querySelector("script"));
